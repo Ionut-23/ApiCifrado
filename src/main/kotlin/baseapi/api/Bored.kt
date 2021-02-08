798ae3c2cb6261e8d34a8651a52b3e735ae32fde
AlumnoM <ionut.epure.ige@gmail.com>
2021-02-08 13:14:31 +0100

    commit2

--- src/main/kotlin/baseapi/api/Bored.kt
+++ src/main/kotlin/baseapi/api/Bored.kt
@@ -1,10 +1,32 @@
 package baseapi.api
 
 import org.springframework.web.bind.annotation.*
+import java.util.*
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
 
+@Entity
+class Bored (var actividad: String, var precio : Int) {
+    @Id
+    @GeneratedValue
+    private val id : Long? = null
 
-class Bored () {
+    override fun equals(other: Any?): Boolean {
+        if(other is Bored){
+            return other.id == id && id != null
+        }else{
+            return false
+        }
+    }
 
+    override fun hashCode(): Int {
+        return Objects.hash(id, actividad, precio)
+    }
+
+    override fun toString(): String {
+        return "$id para esta actividad de $actividad el precio es de $precio"
+    }
 }
 
 /*
--- src/main/kotlin/baseapi/api/LoadDatabase.kt
+++ src/main/kotlin/baseapi/api/LoadDatabase.kt
@@ -2,17 +2,21 @@ package baseapi.api
 
 import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
-import javax.management.loading.ClassLoaderRepository
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
+
+@Configuration
 class LoadDatabase {
     companion object{
         val logger = LoggerFactory.getLogger(LoadDatabase.javaClass)
     }
 
+    @Bean
     fun initDatabase(boredRepository: BoredRepository) : CommandLineRunner{
         return CommandLineRunner { args: Array<String?>? ->
-            logger.info("Preloading " + boredRepository.save())
-            logger.info("Preloading " + boredRepository.save())
+            logger.info("Preloading " + boredRepository.save(Bored("Futbol",30)))
+            logger.info("Preloading " + boredRepository.save(Bored("Natacion",72)))
         }
     }
 }
